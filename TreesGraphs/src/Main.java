eaf65e551f3ecfc79bf50a2c468650fa316d4115
Nathan Nard <nnard1616@gmail.com>
2023-11-07 17:51:30 -0600

    imiplemented correct solution to BST sequence problem

--- TreesGraphs/src/Main.java
+++ TreesGraphs/src/Main.java
@@ -239,8 +239,68 @@ public class Main {
         simpleBtree.left = new TreeNode<Integer>(1);
         simpleBtree.right = new TreeNode<>(3);
 
-        System.out.println(getPossibleOrderings(btree));
+//        System.out.println(getPossibleOrderings(btree));
 
+
+        System.out.println(allSequences(btree));
+
+    }
+
+    private static ArrayList<LinkedList<Integer>> allSequences(TreeNode<Integer> node) {
+        ArrayList<LinkedList<Integer>> result = new ArrayList<LinkedList<Integer>>();
+
+        if (node == null) {
+            result.add(new LinkedList<Integer>());
+            return result;
+        }
+
+        LinkedList<Integer> prefix = new LinkedList<Integer>();
+        prefix.add(node.data);
+
+        /* Recurse on left and right subtrees. */
+        ArrayList<LinkedList<Integer>> leftSeq = allSequences(node.left);
+        ArrayList<LinkedList<Integer>> rightSeq = allSequences(node.right);
+
+        /* Weave together each list from the left and right sides. */
+        for (LinkedList<Integer> left : leftSeq) {
+            for (LinkedList<Integer> right : rightSeq) {
+                ArrayList<LinkedList<Integer>> weaved = new ArrayList<LinkedList<Integer>>();
+                weaveLists(left, right, weaved, prefix);
+                result.addAll(weaved);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Weave lists together in all possible ways. This algorithm works by removing the head
+     * from one list, recursing, and then doing the same thing with the other list.
+     * @param prefix
+     */
+    private static void weaveLists(LinkedList<Integer> first, LinkedList<Integer> second, ArrayList<LinkedList<Integer>> results, LinkedList<Integer> prefix) {
+        /* One list is empty.  add remainder to [a cloned] prefix and store result. */
+        if (first.size() == 0 || second.size() == 0 ) {
+            LinkedList<Integer> result = (LinkedList<Integer>) prefix.clone();
+            result.addAll(first);
+            result.addAll(second);
+            results.add(result);
+            return;
+        }
+        /* recurse with head of first added to the prefix. Removing the head will damage first, so we'll need
+        to put it back where we found it afterwards.
+         */
+        int headFirst = first.removeFirst();
+        prefix.addLast(headFirst);
+        weaveLists(first, second, results, prefix);
+        prefix.removeLast();
+        first.addFirst(headFirst);
+
+        /* Do the same thing with second, damaging and then restoring th elist. */
+        int headSecond = second.removeFirst();
+        prefix.addLast(headSecond);
+        weaveLists(first, second, results, prefix);
+        prefix.removeLast();
+        second.addFirst(headSecond);
     }
 
     private static ArrayList<ArrayList<Integer>> getPossibleOrderings(TreeNode<Integer> input) {
